9ec67aa707794f00379b27ebc5bda4b65c81a90a
AdamVacha <adamrvacha@gmail.com>
2023-08-03 14:02:07 -0400

    encapsulation began

---
+++ src/ButtonMaker.h
@@ -0,0 +1,16 @@
+//
+// Created by Catty on 8/3/2023.
+//
+
+#ifndef TRACE_RACE_BUTTONMAKER_H
+#define TRACE_RACE_BUTTONMAKER_H
+
+
+
+class ButtonMaker {
+
+};
+
+
+
+#endif //TRACE_RACE_BUTTONMAKER_H
